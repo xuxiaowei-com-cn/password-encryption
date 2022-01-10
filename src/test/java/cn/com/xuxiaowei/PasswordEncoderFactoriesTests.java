985936865b226d58bf63600dea5e9b3b66a6cd6f
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-01-10 21:11:29 +0800

    测试

--- src/test/java/cn/com/xuxiaowei/PasswordEncoderFactoriesTests.java
+++ src/test/java/cn/com/xuxiaowei/PasswordEncoderFactoriesTests.java
@@ -4,6 +4,7 @@ import cn.hutool.crypto.SecureUtil;
 import cn.hutool.crypto.digest.MD5;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
@@ -15,11 +16,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 class PasswordEncoderFactoriesTests {
 
     /**
+     * {SoGBcQjzo//jymrVnddobFbeY6vcyDTesL37nXCeL6Y=}7505099ac55f72289339866185816de5
+     * {SnRha+br053FD13/AWf3QHQPxWfz6eK8NA3atnvXyo4=}2242b4e60ffc064ce63a2ea6d72676a6
+     */
+    @Test
+    void md5() {
+        MessageDigestPasswordEncoder md5 = new MessageDigestPasswordEncoder("MD5");
+        String encode = md5.encode("123");
+        System.out.println(encode);
+    }
+
+    /**
      * 加密
      * <p>
      * 与 MD5 加密不同，{@link PasswordEncoder} 每次加密相同原文时，密文不同
      * <p>
      * 123：{bcrypt}$2a$10$KHRctsDUxgu9FVGfPWD3e.5SY4DD0rG1YNVrJ87tZLybwYIu2626y
+     * {bcrypt}$2a$10$Fz6Ke5Ie3H3w/Sjg3FzM4eokaIhbze6GMWQkjfbhj3ZF98bjPFMIu
+     * {bcrypt}$2a$10$BEKzLjdvAZpNSxksTpaEQey0QL3uHg.3aUcdymwTK2FiunRMm6PjK
      */
     @Test
     void encode() {
@@ -37,7 +51,7 @@ class PasswordEncoderFactoriesTests {
     void matches() {
         PasswordEncoder delegatingPasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         boolean matches = delegatingPasswordEncoder.matches("123",
-                "{bcrypt}$2a$10$KHRctsDUxgu9FVGfPWD3e.5SY4DD0rG1YNVrJ87tZLybwYIu2626y");
+                "{bcrypt}$2a$10$Fz6Ke5Ie3H3w/Sjg3FzM4eokaIhbze6GMWQkjfbhj3ZF98bjPFMIu");
         System.out.println("比较结果：" + matches);
     }
 
@@ -51,7 +65,8 @@ class PasswordEncoderFactoriesTests {
         String password = "123";
 
         MD5 md5 = SecureUtil.md5();
-        String digestHex = md5.digestHex(password);
+//        String digestHex = md5.digestHex(password);
+        String digestHex = "202cb962ac59075b964b07152d234b70";
 
         System.out.println("原文：" + password);
         System.out.println("加密结果：" + digestHex);
